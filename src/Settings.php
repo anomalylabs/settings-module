8c4f8064b597b5c6629a80612fb8ea26c62b1bb5
Ryan Thompson <service@aiwebsystems.com>
2014-09-28 10:46:24 -0500

    Allow setting of settings model

--- src/Settings.php
+++ src/Settings.php
@@ -70,17 +70,37 @@ class Settings
      * @param $addon
      * @return mixed
      */
-    protected function getModel($type, $addon)
+    public function getModel($type, $addon)
     {
+        $key = "{$type}.{$addon}";
+
         $namespace = 'Streams\Platform\Model\Settings\\';
 
         $model = $namespace . 'Settings' . studly_case("{$type}_{$addon}") . 'EntryModel';
 
-        if (!isset($this->models[$model])) {
+        if (!isset($this->models[$key])) {
             echo 'New model';
-            $this->models[$model] = new $model;
+            $this->models[$key] = new $model;
         }
 
-        return $this->models[$model];
+        return $this->models[$key];
+    }
+
+    /**
+     * Set an alternate model for a settings stream.
+     *
+     * @param $type
+     * @param $addon
+     * @param $model
+     */
+    public function setModel($type, $addon, $model)
+    {
+        $key = "{$type}.{$addon}";
+
+        if (is_string($model)) {
+            $model = new $model;
+        }
+
+        $this->models[$key] = $model;
     }
 } 
