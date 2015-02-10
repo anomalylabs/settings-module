d828f117f2a875f5fe5bd82becc1d8adf2bd9817
Ryan Thompson <service@aiwebsystems.com>
2015-02-10 03:19:30 -0600

    Testing..

--- src/Setting/SettingRepository.php
+++ src/Setting/SettingRepository.php
@@ -1,5 +1,7 @@
 <?php namespace Anomaly\SettingsModule\Setting;
 
+use Illuminate\Config\Repository;
+
 /**
  * Class SettingRepository
  *
@@ -19,12 +21,39 @@ class SettingRepository
     protected $model;
 
     /**
+     * The config repository.
+     *
+     * @var Repository
+     */
+    protected $config;
+
+    /**
      * Create a new SettingRepository instance.
      *
      * @param SettingModel $model
+     * @param Repository   $config
      */
-    public function __construct(SettingModel $model)
+    public function __construct(SettingModel $model, Repository $config)
     {
-        $this->model = $model;
+        $this->model  = $model;
+        $this->config = $config;
+    }
+
+    /**
+     * Get a setting value.
+     *
+     * @param      $key
+     * @param null $default
+     * @return mixed
+     */
+    public function get($key, $default = null)
+    {
+        $setting = $this->model->where('key', $key)->find();
+
+        if (!$setting) {
+            return $this->config->get($key, $default);
+        }
+
+        return $setting->value;
     }
 }
