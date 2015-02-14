4696403e5869d9db1efaa945d95888e75f2f875b
Ryan Thompson <service@aiwebsystems.com>
2015-02-13 23:11:07 -0600

    Readme

--- src/Setting/SettingModel.php
+++ src/Setting/SettingModel.php
@@ -20,4 +20,48 @@ class SettingModel extends SettingsSettingsEntryModel
      */
     protected $cacheMinutes = 30;
 
+    /**
+     * Set a field value.
+     *
+     * @param $fieldSlug
+     * @param $value
+     */
+    public function setFieldValue($fieldSlug, $value)
+    {
+        $assignment = $this->getAssignment($fieldSlug);
+
+        $type = $assignment->getFieldType($this);
+
+        $accessor = $type->getAccessor();
+        $modifier = $type->getModifier();
+
+        $this->setRawAttributes($accessor->set($this->getAttributes(), $modifier->modify($value)));
+    }
+
+    /**
+     * Get a field value.
+     *
+     * @param      $fieldSlug
+     * @param bool $decorate
+     * @return mixed
+     */
+    public function getFieldValue($fieldSlug, $decorate = false)
+    {
+        $assignment = $this->getAssignment($fieldSlug);
+
+        $type = $assignment->getFieldType($this);
+
+        $accessor = $type->getAccessor();
+        $modifier = $type->getModifier();
+
+        $value = $modifier->restore($accessor->get($this->getAttributes(), $fieldSlug));
+
+        if (!$decorate) {
+            return $value;
+        }
+
+        $type->setValue($value);
+
+        return $type->getPresenter();
+    }
 }
