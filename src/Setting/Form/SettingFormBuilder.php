1b6ced11f5dbc3e4ff7bba67cf1567d1a31a92d4
Ryan Thompson <service@aiwebsystems.com>
2015-03-27 16:57:01 -0500

    Permissions test

--- src/Setting/Form/SettingFormBuilder.php
+++ src/Setting/Form/SettingFormBuilder.php
@@ -1,5 +1,6 @@
 <?php namespace Anomaly\SettingsModule\Setting\Form;
 
+use Anomaly\Streams\Platform\Ui\Form\Form;
 use Anomaly\Streams\Platform\Ui\Form\FormBuilder;
 
 /**
@@ -38,4 +39,20 @@ class SettingFormBuilder extends FormBuilder
      */
     protected $fields = 'Anomaly\SettingsModule\Setting\Form\SettingFormFields@handle';
 
+    /**
+     * Create a new SettingFormBuilder instance.
+     *
+     * @param Form $form
+     */
+    public function __construct(Form $form)
+    {
+        $form->on(
+            'building',
+            function (Form $form) {
+                $form->setOption('permission', $this->entry . '::settings');
+            }
+        );
+
+        parent::__construct($form);
+    }
 }
