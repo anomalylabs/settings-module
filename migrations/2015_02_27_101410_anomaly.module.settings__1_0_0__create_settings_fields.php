051506de724405a6aadfa19ab513fa2c5301060c
Ryan Thompson <service@aiwebsystems.com>
2015-04-23 17:52:19 -0500

    Progress

--- migrations/2015_02_27_101410_anomaly.module.settings__1_0_0__create_settings_fields.php
+++ migrations/2015_02_27_101410_anomaly.module.settings__1_0_0__create_settings_fields.php
@@ -9,7 +9,7 @@ use Anomaly\Streams\Platform\Database\Migration\Migration;
  * @author        AnomalyLabs, Inc. <hello@anomaly.is>
  * @author        Ryan Thompson <ryan@anomaly.is>
  */
-class AnomalyModuleSettingsCreateSettingsFields extends Migration
+class AnomalyModuleSettings_1_0_0_CreateSettingsFields extends Migration
 {
 
     /**
