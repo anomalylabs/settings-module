6eb5f2186b76239f26688ac6c2b1fc0b7fa3b569
Jack Sun <sunhsilu@gmail.com>
2015-11-23 16:37:58 +0800

    Add chinese traditional translation.

---
+++ resources/lang/zh-tw/addon.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'title'       => '設定',
+    'name'        => '設定模組',
+    'description' => '管理系統的設定。',
+    'section'     => [
+        'system'      => '系統',
+        'plugins'     => '程式插件',
+        'modules'     => '程式模組',
+        'extensions'  => '程式擴充',
+        'field_types' => '欄位型別'
+    ]
+];
